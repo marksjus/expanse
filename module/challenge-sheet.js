f2f2fc95b0003943e771451ac895261805a5237a
marksjus <69105636+marksjus@users.noreply.github.com>
2025-07-23 15:46:05 +0200

    Add chase participants

--- module/challenge-sheet.js
+++ module/challenge-sheet.js
@@ -46,7 +46,33 @@ export class ExpanseChallengeSheet extends foundry.appv1.sheets.ActorSheet {
         //fetch consequences
         sheetData.consequences = sheetData.items.filter(i => i.type === "consequence");
         sheetData.ActiveConsequences = sheetData.items.filter(i => i.type === "consequence" && i.system.active == true);
-        console.log(sheetData.ActiveConsequences);
+        
+        //participants
+        const passengers = sheetData.system.participants;
+        let invalidPassengers = [];
+        for (let pi = 0; pi < passengers.length; pi++) {
+            const p = passengers[pi];
+            if (!game.actors) {
+                game.postReadyPrepare.push(this);
+            } else {
+                const pData = p.isToken ? game.actors.tokens[p.id] : game.actors.get(p.id);
+                if (!pData) {
+                    invalidPassengers.push(pi);
+                } else {
+                    p.name = pData.name;
+                    p.picture = pData.prototypeToken.texture.src;
+                    p.speed = pData.system.attributes.speed.modified;
+                    p.successThreshold = sheetData.system.successThreshold;
+                };
+            }
+        }
+        // Remove passengers whose sheets/tokens are not valid anymore
+        for (let ip = 0; ip < invalidPassengers.length; ip++) {
+            const i = invalidPassengers[ip];
+            passengers.splice(i, 1);
+        };
+        
+        console.log(sheetData.system.participants);
         return sheetData;
     }
 
@@ -80,6 +106,9 @@ export class ExpanseChallengeSheet extends foundry.appv1.sheets.ActorSheet {
         // Create Item
         html.find(".item-create").click(this._itemCreate.bind(this));
 
+        html.find(".participant-delete").click(this._onRemovePassenger.bind(this));
+        html.find(".chase-mod").click(this._onClickChase.bind(this));
+        html.find('.chase-position').change(this._onChangeChase.bind(this));
         
         html.find(".item-update-checkbox").click((ev) => {
             let itemId = $(ev.currentTarget).parents(".item").attr("data-item-id");
@@ -101,6 +130,10 @@ export class ExpanseChallengeSheet extends foundry.appv1.sheets.ActorSheet {
         
     }
 
+    async _onDrop(event) {
+        return this.dropChar(event, this.actor) ? null : super._onDrop(event);
+    }
+
     _itemCreate(event) {
         event.preventDefault();
         const header = event.currentTarget;
@@ -112,5 +145,72 @@ export class ExpanseChallengeSheet extends foundry.appv1.sheets.ActorSheet {
         };
         delete itemData.data.type;
         return this.actor.createEmbeddedDocuments("Item", [itemData], { renderSheet: true });
+    }
+
+    _onRemovePassenger(event) {
+        let update = {};
+        let participantKey = $(event.currentTarget).parents(".item").attr("data-item-key");
+        participantKey = Number(participantKey);
+        const crew = this.object.system.participants;
+        crew.splice(participantKey, 1);
+        this.actor.update({...update, "system.participants": crew});
+    };
+    
+    dropChar(event, vehicle) {
+        const dragData = JSON.parse(event.dataTransfer.getData("text/plain"))
+        const data = fromUuidSync(dragData.uuid);
+        if (data.documentName === "Actor") {
+            if (data.type === "character" || data.type === "ship") {
+                const passengerData = {
+                    id : data.id,
+                    isToken : data.isToken,
+                    chasePosition: 0
+                };
+                const passengerList = vehicle.system.participants;
+                let alreadyOnboard = false;
+                passengerList.map( p => {
+                    if (p.id === passengerData.id) {
+                        alreadyOnboard = true;
+                        const parts = {name: p.name, id: p.id};
+                        let warning = game.i18n.format("EXPANSE.WARNING.alreadyOnboard", parts);
+                        ui.notifications.warn(warning);
+                    }
+                });
+
+                if (!alreadyOnboard) {
+                    passengerList.push(passengerData);
+                    vehicle.update({"system.participants" : passengerList});
+                }
+            } else {
+                const warning = game.i18n.localize("EXPANSE.WARNING.ThisIsNotValidParticipant");
+                ui.notifications.warn(warning);
+            }
+        } else return false;
+        return true;
+    }
+
+    async _onClickChase(event){
+        event.preventDefault();
+        const participants = foundry.utils.duplicate(this.actor.system.participants);
+        let participantKey = $(event.currentTarget).parents(".item").attr("data-item-key");
+        participantKey = Number(participantKey);
+
+        (event.currentTarget.classList.contains('minus')) ? participants[participantKey].chasePosition-- : participants[participantKey].chasePosition++;
+        if(participants[participantKey].chasePosition.chasePosition < 0) participants[participantKey].chasePosition.chasePosition = 0;
+        if(participants[participantKey].chasePosition.chasePosition > this.actor.system.successThreshold) participants[participantKey].chasePosition.chasePosition = this.actor.system.successThreshold;
+
+        await this.actor.update({ system: { participants: participants } });
+    }
+
+    async _onChangeChase(event){
+        event.preventDefault();
+        const value = event.currentTarget.value;
+        const participants = foundry.utils.duplicate(this.actor.system.participants);
+        let participantKey = $(event.currentTarget).parents(".item").attr("data-item-key");
+        participantKey = Number(participantKey);
+
+        participants[participantKey].chasePosition = value;    
+
+        await this.actor.update({ system: { participants: participants } });
     }
 }
